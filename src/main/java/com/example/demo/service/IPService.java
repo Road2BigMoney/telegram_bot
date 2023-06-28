f69b675dfc72512eac29de3ca8d49e5fcf892cb9
Рамазан Гаджиев <mm.gadzhiev@mail.ru>
2023-06-29 02:07:13 +0300

    init commit

---
+++ src/main/java/com/example/demo/service/IPService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import com.example.demo.model.IPModel;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+public class IPService {
+    public static String getIP(String message, IPModel model) throws IOException {
+        URL url = new URL("https://api.ipify.org/?format=json");
+        Scanner scanner = new Scanner((InputStream) url.getContent());
+        String result = "";
+        while (scanner.hasNext()){
+            result +=scanner.nextLine();
+        }
+        JSONObject object = new JSONObject(result);
+        model.setIP(object.getString("ip"));
+
+        return "Ваш ip : " + model.getIP();
+
+    }
+}
